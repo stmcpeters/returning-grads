2b68a20a08f8b9deaf730d948ef8d65f82c56bdc
stmcpeters <stmcpeters@gmail.com>
2024-12-26 23:45:40 -0800

    installs pandas library and includes assignment instructions

---
+++ python-student-grades/scripts.py
@@ -0,0 +1,20 @@
+# used 'pip install pandas' to install panda 
+# imports pandas library to be used 
+import pandas as pd
+
+# read_csv() function is used to read data from a CSV file into a Pandas DataFrame
+# DataFrame allows you to manipulate and analyze table data easily
+df = pd.read_csv('grades.csv')
+# prints CSV table data - aka student grades
+#print(df)
+
+
+# Calculate the average grade for each subject across all students.
+
+# Find the student(s) with the highest overall grade (average of all three subjects).
+
+# Determine which subject has the highest average grade overall.
+
+# Create a simple report that summarizes these findings.
+# Use Python's string formatting capabilities to present the results in a readable format.
+
