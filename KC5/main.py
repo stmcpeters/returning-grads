016e4ac7210a8fa778cd9ec7af88e553fa06b5ae
stmcpeters <stmcpeters@gmail.com>
2025-02-19 21:41:49 -0800

    add error handling and database connection closure for API data fetching

--- KC5/main.py
+++ KC5/main.py
@@ -9,6 +9,7 @@ import json
 url = 'https://www.nytimes.com/section/technology'
 api_url = 'https://official-joke-api.appspot.com/jokes/programming/ten'
 
+################## fetching and storing data from jokes API ##################
 try:
   # uses requests module to make a HTTP request to the API
   api_response = requests.get(api_url)
@@ -56,10 +57,13 @@ try:
       print(result)
       # close the connection
       conn.close()
-
-
   else:
       print("Error:", api_response.status_code, api_response.text)
+
+############# error handling ###############
+# error handling for HTTP requests
+except requests.exceptions.RequestException as e:
+  print(f'error fetching data from {url}: {e}')
 
 # error handling for sqlite 
 except sqlite3.Error as e:
@@ -69,78 +73,86 @@ except sqlite3.Error as e:
 except Exception as e:
   print(f'an error occurred: {e}')
 
-# try:
-# ############ webscraping NYT articles ################
-#   # uses the requests module to fetch HTML content from specified URL
-#   response = requests.get(url)
+########### close database connection ############
+# closes connection to database whether data is successful or error is thrown
+finally:
+  if 'connection' in locals():
+    conn.close()
 
-#   # parse the HTML content using BeautifulSoup
-#   soup = BeautifulSoup(response.content, "html.parser")
-#   # print(soup.prettify())
+##########################################################################################
 
-#   # find all elements needed to display articles (title, paragraph and links)
-#   titles = soup.find_all('h3', class_="css-1j88qqx e15t083i0")
-#   # for title in titles:
-#   #   print(title.text)
-#   descriptions = soup.find_all('p', class_="css-1pga48a e15t083i1")
-#   # for description in descriptions:
-#   #   print(description.text)
-#   links = soup.find_all('a', class_="css-8hzhxf")
-#   # for link in links:
-#   #   print(link.get('href'))
+############ webscraping NYT articles ################
+try:
+  # uses the requests module to fetch HTML content from specified URL
+  response = requests.get(url)
 
-# # ######### creating database and table ############
-#   # create connection to sqlite database, if it doesn't exist it will be created
-#   connection = sqlite3.connect('tech_news.db')
-#   # create a cursor object to execute SQL queries
-#   cursor = connection.cursor()
+  # parse the HTML content using BeautifulSoup
+  soup = BeautifulSoup(response.content, "html.parser")
+  # print(soup.prettify())
 
-#   # drop table if it already exists
-#   cursor.execute('DROP TABLE IF EXISTS articles')
+  # find all elements needed to display articles (title, paragraph and links)
+  titles = soup.find_all('h3', class_="css-1j88qqx e15t083i0")
+  # for title in titles:
+  #   print(title.text)
+  descriptions = soup.find_all('p', class_="css-1pga48a e15t083i1")
+  # for description in descriptions:
+  #   print(description.text)
+  links = soup.find_all('a', class_="css-8hzhxf")
+  # for link in links:
+  #   print(link.get('href'))
 
-#   # articles database schema
-#   table = '''CREATE TABLE articles (
-#               ID INTEGER PRIMARY KEY AUTOINCREMENT,
-#               title TEXT,
-#               description TEXT,
-#               article_link TEXT
-#               );'''
-#   # creates table in database
-#   cursor.execute(table)
-#   # 'commits' table being created
-#   connection.commit()
-#   # message to console showing table has been created
-#   print('articles table has been created')
+# ######### creating database and table ############
+  # create connection to sqlite database, if it doesn't exist it will be created
+  connection = sqlite3.connect('tech_news.db')
+  # create a cursor object to execute SQL queries
+  cursor = connection.cursor()
 
-#   # iterate through data and insert into articles table
-#   # zip() inserts all data in one command (best practice for matching data)
-#   for title, description, article_link in zip(titles, descriptions, links):
-#     cursor.execute('''INSERT INTO articles (title, description, article_link) VALUES (?, ?, ?)''', (title.text, description.text, article_link.get('href')))
-#   # commit changes
-#   connection.commit()
+  # drop table if it already exists
+  cursor.execute('DROP TABLE IF EXISTS articles')
 
-#   # select all data from articles table
-#   cursor.execute('''SELECT * FROM articles''')
-#   # fetch data from table
-#   result = cursor.fetchall()
-#   # message in console to confirm database data has been fetched
-#   print(f'database data has been fetched: {result}')
+  # articles database schema
+  table = '''CREATE TABLE articles (
+              ID INTEGER PRIMARY KEY AUTOINCREMENT,
+              title TEXT,
+              description TEXT,
+              article_link TEXT
+              );'''
+  # creates table in database
+  cursor.execute(table)
+  # 'commits' table being created
+  connection.commit()
+  # message to console showing table has been created
+  print('articles table has been created')
 
-# ############# error handling ###############
-# # error handling for HTTP requests
-# except requests.exceptions.RequestException as e:
-#   print(f'error fetching data from {url}: {e}')
+  # iterate through data and insert into articles table
+  # zip() inserts all data in one command (best practice for matching data)
+  for title, description, article_link in zip(titles, descriptions, links):
+    cursor.execute('''INSERT INTO articles (title, description, article_link) VALUES (?, ?, ?)''', (title.text, description.text, article_link.get('href')))
+  # commit changes
+  connection.commit()
 
-# # error handling for sqlite 
-# except sqlite3.Error as e:
-#   print(f'database error: {e}')
+  # select all data from articles table
+  cursor.execute('''SELECT * FROM articles''')
+  # fetch data from table
+  result = cursor.fetchall()
+  # message in console to confirm database data has been fetched
+  print(f'database data has been fetched: {result}')
 
-# # catch all error handling
-# except Exception as e:
-#   print(f'an error occurred: {e}')
+############# error handling ###############
+# error handling for HTTP requests
+except requests.exceptions.RequestException as e:
+  print(f'error fetching data from {url}: {e}')
 
-# ########### close database connection ############
-# # closes connection to database whether data is successful or error is thrown
-# finally:
-#   if 'connection' in locals():
-#     connection.close()
+# error handling for sqlite 
+except sqlite3.Error as e:
+  print(f'database error: {e}')
+
+# catch all error handling
+except Exception as e:
+  print(f'an error occurred: {e}')
+
+########### close database connection ############
+# closes connection to database whether data is successful or error is thrown
+finally:
+  if 'connection' in locals():
+    connection.close()
